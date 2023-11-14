7d899a71aba035309dd3949a5ee1252d2236b216
personalHealth <147271348+personalHealth@users.noreply.github.com>
2023-11-13 21:39:06 -0300

    Update responisve button

--- src/components/ButtonsZapping.jsx
+++ src/components/ButtonsZapping.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function ButtonsZapping(id) {
   const currentChannel = () => {
@@ -10,12 +11,12 @@ export default function ButtonsZapping(id) {
   };
 
   return (
-    <div className="flex absolute bottom-3 right-60 ">
+    <div className="flex absolute bottom-60 right-24 sm:bottom-12 sm:right-64 lg:bottom-3">
       <Link
         className="h-9 gap-3 flex justify-center items-center w-24 px-1 py-1 bg-sky-500 rounded text-lg mr-3"
         href={`/transmision/${currentChannel() - 1}`}
       >
-        <img
+        <Image
           width="25"
           height="25"
           src="https://img.icons8.com/ios/25/circled-chevron-left.png"
@@ -28,7 +29,7 @@ export default function ButtonsZapping(id) {
         href={`/transmision/${currentChannel() + 1}`}
       >
         SIG.
-        <img
+        <Image
           width="25"
           height="25"
           src="https://img.icons8.com/ios/50/circled-chevron-right--v1.png"
