8b362e579c1f63c8111653bafd23464a7aba1319
personalHealth <147271348+personalHealth@users.noreply.github.com>
2023-12-24 03:16:24 -0300

    Update Control.jsx

--- src/components/Control.jsx
+++ src/components/Control.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { allchannels } from "@/components/Channels";
+import { allchannels } from "@/app/page";
 import Link from "next/link";
 
 export default function Control() {
