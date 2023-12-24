7f983abae2d88364f7e2e43ad8f77d121ce9cfc6
personalHealth <147271348+personalHealth@users.noreply.github.com>
2023-12-24 03:00:45 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,25 @@
 import React from "react";
+import getChannels from "@/app/data/chanels";
 import NavBar from "@/components/NavBar";
 import Channels from "@/components/Channels";
 import Footer from "@/components/Footer";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Sign from "@/app/sign/page";
 
-export default function Home() {
+export default async function Home() {
   const { getUser, isAuthenticated } = getKindeServerSession();
+  const allchannels = await getChannels();
   const user = getUser();
   const auth = isAuthenticated();
 
   // if (!auth) {
   //   return <Sign />;
   // } else
-    return (
-      <main className="bg-slate-600 bg-cover">
-        <NavBar />
-        <Channels />
-        <Footer />
-      </main>
-    );
+  return (
+    <main className="bg-slate-600 bg-cover">
+      <NavBar />
+      <Channels allchannels={allchannels} />
+      <Footer />
+    </main>
+  );
 }
